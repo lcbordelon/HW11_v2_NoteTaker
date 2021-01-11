c216516fc28baf0484b1a5ad30140b8822059f9b
Lindsay Bordelon <lcbordelon@gmail.com>
2021-01-10 19:39:54 -0700

    html routes working.

---
+++ Develop/data/notes.js
@@ -0,0 +1,8 @@
+var notesArray = [
+  {
+    noteTitle: "meowmeow",
+    noteText: "I need to feed the cats later.",
+  },
+];
+
+module.exports = notesArray;
---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+require("./routes/html-routes.js")(app);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
