1d9dac4a4cc0f4a69080953b6547fa196af16313
Lindsay Bordelon <lcbordelon@gmail.com>
2021-02-21 11:07:02 -0700

    updated API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,66 @@
-const db = require("../db/db.json");
-const app = require("express");
-
-//function to save new note to db.json
+const fs = require("fs");
 
 module.exports = (app) => {
-  app.post("/api/notes", (req, res) => {
-    const newNote = req.body;
-    newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
-    console.log(newNote);
+  //GET all notes
+  app.get("/api/notes", (req, res) => {
+    fs.readFile(__dirname + "/../db/db.json", "utf8", (error, text) => {
+      if (error) {
+        console.log(error);
+      }
+      res.json(JSON.parse(text));
+    });
+  });
 
-    db.push(newNote);
-    res.json(newNote);
+  //POST new note
+  app.post("/api/notes", (req, res) => {
+    fs.readFile(__dirname + "/../db/db.json", "utf8", (error, text) => {
+      if (error) {
+        console.log(error);
+      }
+      text = JSON.parse(text);
+      req.body.id = Date.now();
+      text.push(req.body);
+      fs.writeFile(
+        __dirname + "/../db/db.json",
+        JSON.stringify(text),
+        (error) => {
+          if (error) {
+            console.log(error);
+          }
+          res.json(true);
+        }
+      );
+    });
+  });
+
+  //DELETE note
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile(__dirname + "/../db/db.json", "utf8", (error, text) => {
+      if (error) {
+        console.log(error);
+      }
+      text = JSON.parse(text);
+
+      const notesArray = [];
+      const id = req.params.id;
+      for (let i = 0; i < text.length; i++) {
+        const currentNoteId = text[i].id;
+
+        console.log(currentNoteId);
+
+        if (currentNoteId != id) {
+          notesArray.push(text[i]);
+        }
+      }
+
+      let filteredNotes = JSON.stringify(notesArray);
+
+      fs.writeFile(__dirname + "/../db/db.json", filteredNotes, (error) => {
+        if (error) {
+          console.log(error);
+        }
+        res.json(true);
+      });
+    });
   });
 };
